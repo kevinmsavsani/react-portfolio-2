1296f47f23d1ddde526be09af12bf1f0ef544fba
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-30 11:00:52 +0530

    first commit

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
           >
             <img
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              alt="Kevin portrait"
               width="192"
               height="192"
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
@@ -59,11 +59,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Kevin.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React (Remix)</span>.
       </motion.h1>
 
       <motion.div
